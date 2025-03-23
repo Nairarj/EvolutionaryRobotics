4aa9122f054d12a0e2b86b4804e419238d34847e
Nairarj <123185453+Nairarj@users.noreply.github.com>
2025-03-23 17:17:58 -0400

    Finished A09, final constants variable tweaks

--- constants_py.py
+++ constants_py.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 #constants.py
-iterations = 300
-backAmplitude = np.pi / 4
+iterations = 450
+backAmplitude = np.pi / 8
 backFrequency = 5
 backPhaseOffset = 0
 
 
-frontAmplitude = np.pi / 2
+frontAmplitude = np.pi / 4
 frontFrequency = 5
 frontPhaseOffset = np.pi/2
 
-numberOfGenerations = 5
-populationSize = 5
+numberOfGenerations = 6
+populationSize = 3
